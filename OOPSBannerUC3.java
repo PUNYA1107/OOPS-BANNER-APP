0439dbe8abd8dfc121772ee915f4b538bc9eb6d9
PUNYA1107 <punyatoyabiswal07@gmail.com>
2026-02-17 10:38:30 +0530

    UC3: Refactor OOPS Banner UC3 using String.join

---
+++ OOPSBannerUC3.java
@@ -0,0 +1,12 @@
+public class OOPSBannerUC3 {
+    public static void main(String[] args) {
+        // UC2: Large OOPS BannerUC3 using 7-line format and 9-character width
+        System.out.println(String.join("","  ***** " , "   ***** " , "  ***** " , "  ***** "));
+        System.out.println(String.join(""," *     * " , " *     * " , " *    * " , " *     "));
+        System.out.println(String.join(""," *     * "  ," *     * " , " *    * " , " *      "));
+        System.out.println(String.join(""," *     * "  ," *     * " , " ***** " , "  ***** "));
+        System.out.println(String.join(""," *     * " , " *     * " , " * " , "          * "));
+        System.out.println(String.join(""," *     * " , " *     * " , " * " , "          * "));
+        System.out.println(String.join("","  ***** " , "   *****  "  , " * " , "      ***** "));
+    }
+}
